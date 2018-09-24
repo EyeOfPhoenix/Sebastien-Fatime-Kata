ea01339e3b272291280987e1997927373fd6dbe6
Seb <sebastien.fatime@gmail.com>
2018-09-24 03:20:13 +0200

    Account should has an id.

--- src/main/java/fr/sgcib/fatime/kata/account/domain/Account.java
+++ src/main/java/fr/sgcib/fatime/kata/account/domain/Account.java
@@ -1,6 +1,11 @@
 package fr.sgcib.fatime.kata.account.domain;
 
 
+import lombok.Builder;
+import lombok.Value;
 
+@Value
+@Builder
 public class Account {
+    Long id;
 }
--- src/test/java/fr/sgcib/fatime/kata/account/domain/AccountTest.java
+++ src/test/java/fr/sgcib/fatime/kata/account/domain/AccountTest.java
@@ -1,5 +1,14 @@
-import static org.junit.Assert.*;
+package fr.sgcib.fatime.kata.account.domain;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsInstanceOf.instanceOf;
+import static org.junit.Assert.assertThat;
 
 public class AccountTest {
 
+    @Test
+    public void should_has_an_id() {
+        assertThat(Account.builder().id(10000L).build().getId(), instanceOf(Long.class));
+    }
 }
