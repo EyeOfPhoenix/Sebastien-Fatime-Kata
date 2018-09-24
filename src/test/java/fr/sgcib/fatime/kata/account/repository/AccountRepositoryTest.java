3a7b7aaa9fd4f565a8a5930cdb9d71beb25207b0
Seb <sebastien.fatime@gmail.com>
2018-09-24 12:07:58 +0200

    accountRepository fetch an account by number account.

---
+++ src/test/java/fr/sgcib/fatime/kata/account/repository/AccountRepositoryTest.java
@@ -0,0 +1,61 @@
+package fr.sgcib.fatime.kata.account.repository;
+
+import fr.sgcib.fatime.kata.account.domain.Account;
+import fr.sgcib.fatime.kata.account.domain.Customer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountRepositoryTest {
+    @Autowired
+    AccountRepository accountRepository;
+
+    private Account expectedAccount;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Before
+    public void init() {
+        expectedAccount = Account.builder()
+                .id(1L)
+                .number("A123456A")
+                .solde(1000L)
+                .customer(Customer.builder()
+                        .id(1L)
+                        .firstname("Sébastien")
+                        .name("Fatime")
+                        .build())
+                .build();
+
+        Customer customer = Customer.builder().firstname("Sébastien").name("Fatime").build();
+        Account account = Account.builder().number("A123456A").solde(1000L).build();
+
+        customer.setAccount(account);
+        account.setCustomer(customer);
+
+        testEntityManager.persist(account);
+    }
+
+    @Test
+    public void should_fetch_account_by_number() {
+        Account account = accountRepository.findByNumber("A123456A");
+
+        assertThat(account).isNotNull();
+        assertThat(account.getId()).isEqualTo(expectedAccount.getId());
+        assertThat(account.getNumber()).isEqualTo(expectedAccount.getNumber());
+        assertThat(account.getSolde()).isEqualTo(expectedAccount.getSolde());
+        assertThat(account.getCustomer().getId()).isEqualTo(expectedAccount.getCustomer().getId());
+        assertThat(account.getCustomer().getFirstname()).isEqualTo(expectedAccount.getCustomer().getFirstname());
+        assertThat(account.getCustomer().getName()).isEqualTo(expectedAccount.getCustomer().getName());
+    }
+}
