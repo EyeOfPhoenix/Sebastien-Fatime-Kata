42c5df03b834a21020e0925f3669ee571e4f2f6e
Seb <sebastien.fatime@gmail.com>
2018-09-24 05:40:17 +0200

    Persist Account and Customer.

--- src/main/java/fr/sgcib/fatime/kata/account/domain/Account.java
+++ src/main/java/fr/sgcib/fatime/kata/account/domain/Account.java
@@ -1,14 +1,31 @@
 package fr.sgcib.fatime.kata.account.domain;
 
 
+import com.sun.istack.internal.NotNull;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
 
-@Value
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Data
 @Builder
 public class Account {
-    Long id;
-    String number;
-    Long solde;
-    Customer customer;
+
+    @Id
+    @GeneratedValue
+    private final Long id;
+
+    @NotNull
+    private final String number;
+
+    private final Long solde;
+
+    @OneToOne(fetch = LAZY)
+    private final Customer customer;
 }
--- src/main/java/fr/sgcib/fatime/kata/account/domain/Customer.java
+++ src/main/java/fr/sgcib/fatime/kata/account/domain/Customer.java
@@ -4,12 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
 
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
 @Value
 @Builder
 @AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue
     Long id;
+
     String name;
     String firstname;
+
+    @OneToOne(fetch = LAZY)
     Account account;
 }
