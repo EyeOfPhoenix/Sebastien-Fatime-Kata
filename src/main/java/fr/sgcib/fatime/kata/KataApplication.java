8bdde4758b6974e5755edcad9bd1a47d18294115
Seb <sebastien.fatime@gmail.com>
2018-09-25 02:03:23 +0200

    A customer can make a deposit in his account.

--- src/main/java/fr/sgcib/fatime/kata/KataApplication.java
+++ src/main/java/fr/sgcib/fatime/kata/KataApplication.java
@@ -1,12 +1,42 @@
 package fr.sgcib.fatime.kata;
 
+import fr.sgcib.fatime.kata.account.business.AccountService;
+import fr.sgcib.fatime.kata.account.domain.Account;
+import fr.sgcib.fatime.kata.account.domain.Deposit;
+import fr.sgcib.fatime.kata.account.exception.AccountNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class KataApplication {
+
+@ComponentScan({"fr.sgcib.fatime.kata.account"})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@EnableJpaRepositories
+public class KataApplication implements CommandLineRunner {
+    @Autowired
+    private AccountService accountService;
 
     public static void main(String[] args) {
         SpringApplication.run(KataApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        //*********//
+        //   US1   //
+        //*********//
+
+        if (args.length == 2) {
+            String accountNumber = args[0];
+            Deposit deposit = new Deposit(Long.valueOf(args[1]));
+            Account account = accountService.fetchAccountByNumber(accountNumber)
+                    .orElseThrow(() -> new AccountNotFoundException(String.format("The account %d not found!", accountNumber)));
+
+            accountService.depose(deposit, account);
+        }
     }
 }
